ea3d41d4f826d22b18d468c947080cb634c2baed
Jacob Padilla <jp@jacobpadilla.com>
2025-02-07 13:37:53 -0500

    getCatGif function

--- popup.js
+++ popup.js
@@ -1,1 +1,18 @@
-console.log("This is a popup!")
+const apiKey = ''
+const URL = `https://api.giphy.com/v1/gifs/random?api_key=${apiKey}&tag=cat&rating=g`
+
+async function getCatGif() {
+    try {
+        const response = await fetch(URL)
+        console.log(response.ok)
+        const data = await response.json()
+        const imageUrl = data.data.images.original.url
+        
+        document.getElementById("cat-gif").src = imageUrl
+    } catch (error) {
+        console.error("Error fetching cat GIF:", error)
+    }
+}
+
+getCatGif()
+
